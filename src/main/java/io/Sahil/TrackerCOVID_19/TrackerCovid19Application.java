f87659ca8f82a5db8923125c4e0bb42e72b10067
SMak06 <makwane.work@gmail.com>
2020-04-01 13:53:19 +0530

    Initial Commit

---
+++ src/main/java/io/Sahil/TrackerCOVID_19/TrackerCovid19Application.java
@@ -0,0 +1,15 @@
+//Understand that Java is a pain in the A to work with; so make sure the version of Java is the most recent one available. When exporting the ZIP file from start.spring.io, make sure to select the latest Java version as well as download the latest JDK for your system. Sigh!
+
+package io.Sahil.TrackerCOVID_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerCovid19Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/Sahil/TrackerCOVID_19/TrackerCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Sahil.TrackerCOVID_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerCovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
