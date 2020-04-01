9742c43dead6c9f8c3167f849580e25177d8e084
SMak06 <makwane.work@gmail.com>
2020-04-01 16:13:11 +0530

    Added Html table display

--- src/main/java/io/Sahil/TrackerCOVID_19/controllers/HomeController.java
+++ src/main/java/io/Sahil/TrackerCOVID_19/controllers/HomeController.java
@@ -1,14 +1,21 @@
 package io.Sahil.TrackerCOVID_19.controllers;
 
 
+import io.Sahil.TrackerCOVID_19.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
     @GetMapping("/") //for returning the home template [uses thymeleaf dependency]
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
         return "home";
     }
 }
